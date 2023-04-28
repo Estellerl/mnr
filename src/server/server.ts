6d84d5f946be4267b433ecbfdcc18df9f8089973
Estellerl <estellerl@icloud.com>
2023-04-28 09:32:58 +0100

    1. Middleware and Templating

--- src/server/server.ts
+++ src/server/server.ts
@@ -1,3 +1,21 @@
 import express from "express" ;
 
-console.log(express);
+
+const server = express();
+
+// add middleware 
+server.use(express.static("dist"));
+
+server.set("view engine","ejs" );
+
+server.use("/",(req, res) => {
+    res.render("index", {
+        content: "EJS is cool"
+    });
+});
+
+// takes three arguments: 1.The port that it will be listing to 2.Machine host (IP of the machine) 3. function that will be exicuted when the server is done mounting itself off that port 
+server.listen(8080, "0.0.0.0" , () => { console.info(
+    "Express server is listening at http://0.0.0.0:8080"
+);
+})
