57b9d388ed70b11aa7f610a1dbf60fc6c6d60347
Estellerl <estellerl@icloud.com>
2023-05-05 10:24:21 +0100

    1. MongoDB and Docker

--- src/server/config.ts
+++ src/server/config.ts
@@ -2,12 +2,17 @@ console.log("Config ");
 
 const env = process.env;
 
-export var PORT = env.PORT ?? 8080;
-export const HOST = env.HOST ?? "0.0.0.0"
-export const SERVER_URL = `http://${HOST}:${PORT}`
+
+export const PORT = env.PORT ?? 8080;
+export const HOST = env.HOST ?? "0.0.0.0";
+export const SERVER_URL = `http://${HOST}:${PORT}`;
+
+export const MANGODB_URI = env.MANGODB_URI ?? "mongodb://localhost:27017";
+export const DATABASE_NAME = env.DATABASE_NAME ?? "local"; 
 
 export default {
     PORT,
     HOST,
     SERVER_URL,
+    
 };
